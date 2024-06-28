30a00d0003fe8a62f5294f9ca5f78a8d5332e5e7
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2024-06-28 01:57:23 -0700

    feat: adding basic product details page

--- src/app/(pages)/shop/[category]/[slug]/page.tsx
+++ src/app/(pages)/shop/[category]/[slug]/page.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { useParams } from "next/navigation";
+import { productQuery } from "@/utils/productQuery";
+import ProductDetails from "@/components/shop/ProductDetails";
 
 type Props = {};
 
-const page = (props: Props) => {
-  return <div>Slug Page</div>;
+const ProductPage = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = params;
+
+  const response = await fetch(`${process.env.BASE_URL}/api/fetchProducts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      productQuery: productQuery({
+        handle: slug,
+      }),
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  if (!data) {
+    return <div>No product found</div>;
+  }
+
+  const product = data.product;
+
+  return (
+    <div className="z-0 relative min-h-[calc(100vh-70px)] ">
+      <ProductDetails product={product} />
+    </div>
+  );
 };
 
-export default page;
+export default ProductPage;
